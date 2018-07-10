09a2deb4adf37a82dee3a19d20237d1eaf518609
Maria Rosa Carrion <maria.rosa.carrion@gmail.com>
2018-07-10 12:05:57 -0400

    added details and index views

--- bakery_rotation/urls.py
+++ bakery_rotation/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('details/<int:item_id>/', views.details, name='details'),
 ]
--- bakery_rotation/views.py
+++ bakery_rotation/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from .models import BakingSlot
+from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, Gordon!")
+    baking_list = BakingSlot.objects.order_by('-date')
+    context = {
+        'baking_list': baking_list
+    }
+
+    return render(request, 'baking_rotation/index.jinja', context)
+
+
+def details(request, item_id):
+    item = BakingSlot.objects.get(pk=item_id)
+    context = {
+        'item': item
+    }
+
+    return render(request, 'baking_rotation/details.jinja', context)
