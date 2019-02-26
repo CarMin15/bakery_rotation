eed360daeed0ae8cc2ce4b2a91836c40a9b89b13
Gordon Eccles <gmeccles@gmail.com>
2019-02-26 12:39:48 -0500

    Add timesince property

--- bakery_rotation/models.py
+++ bakery_rotation/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -18,3 +20,32 @@ class BakingSlot(models.Model):
 
     class Meta:
         ordering = ['date']
+
+    @property
+    def timesince(self, default="just now"):
+        """
+        Returns string representing "time since" e.g.
+        3 days ago, 5 hours ago etc.
+        """
+        now = datetime.utcnow()
+        diff = now - datetime(
+            self.date.year, self.date.month, self.date.day
+        )
+
+        periods = (
+            (diff.days // 365, "year", "years"),
+            (diff.days // 30, "month", "months"),
+            (diff.days // 7, "week", "weeks"),
+            (diff.days, "day", "days"),
+            (diff.seconds // 3600, "hour", "hours"),
+            (diff.seconds // 60, "minute", "minutes"),
+            (diff.seconds, "second", "seconds"),
+        )
+
+        for period, singular, plural in periods:
+            if period:
+                return "%d %s ago" % (
+                    period, singular if period == 1 else plural
+                )
+
+        return default
