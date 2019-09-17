0f3e7a67d572fed873473b7032b16c93a458c1d0
Gordon <gmeccles@gmail.com>
2019-09-17 12:00:46 -0400

    Change dev static media folder to "./media" for dev

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = BASE_DIR + 'baking_rotation.com/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/baking_rotation/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
